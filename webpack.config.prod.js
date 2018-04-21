ee646bb725a0ca8ca486faf57967bb85309d8580
Peter Lemiszki <plemiszki@gmail.com>
2018-04-21 14:00:41 -0400

    put uglifier back

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -13,11 +13,11 @@ module.exports = {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress:{
-    //     warnings: true
-    //   }
-    // })
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
   ],
   module: {
     loaders: [
