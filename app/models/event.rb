896889768f36bae75468ca2cfbca9e37d814a4f3
Peter Lemiszki <plemiszki@gmail.com>
2018-12-30 09:51:39 -0500

    don't show schedule in menu if no upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,7 @@ class Event < ApplicationRecord
   validates :title, :text, presence: true
   validates_date :time, allow_blank: false
 
+  def self.upcoming
+    where('time > ?', DateTime.now)
+  end
 end
